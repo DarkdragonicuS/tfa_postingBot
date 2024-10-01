5b7458bbd202219fd8c817f9be563c1d558f0e4c
DarkdragonicuS <dark.killer91@hotmail.com>
2024-10-02 10:32:37 +1100

    added md5 output

--- bot.py
+++ bot.py
@@ -18,6 +18,9 @@ from config import TELEGRAM_BOT_TOKEN, E621_API_KEY, E621_API_USERNAME
 from random import shuffle
 import requests
 from global_vars.vars import TAG_SPECIES
+import hashlib
+from requests_toolbelt.multipart import MultipartEncoder
+from io import BytesIO
 
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 dp = Dispatcher(bot)
@@ -29,49 +32,34 @@ async def start(message: types.Message):
 async def reverse_search(image_file_id):
     # Download the image file from Telegram
     image_file = await bot.download_file_by_id(image_file_id)
-
+    image_bytes = BytesIO(image_file.read())
+      
     # Set the API endpoint and parameters
     url = "https://e621.net/iqdb_queries.json"
     auth = requests.auth.HTTPBasicAuth(E621_API_USERNAME,E621_API_KEY)
+    parameters = {
+        "limit": 1
+    }
     headers = {
         "User-Agent": "TFA Posting Bot"
     }
+
     # Send the request
-    response = requests.post(url, auth=auth, headers=headers, files={"file": image_file})
+    response = requests.post(url, auth=auth, headers=headers, files={'file': image_bytes}, params=parameters)
 
     # Check if the response was successful
     if response.status_code == 200:
         # Parse the JSON response
         data = response.json()
         # Return the results
+        image_md5 = hashlib.md5(image_file.read()).hexdigest()
         try:
-            return data[0]["post"]["posts"]
+            return {"md5": image_md5, "posts": data[0]["post"]["posts"]}
         except (KeyError, IndexError):
             return ["Error: Unable to parse response"]
     else:
         # Return an error message
         return ["Error: {}".format(response.status_code)]
-
-# async def tag_categories():
-
-#     # Initialize an empty dictionary to store tags by category
-#     species = []
-
-#     # Read the CSV file
-#     with open('tags.csv', 'r', encoding='utf-8') as csvfile:
-#         reader = csv.DictReader(csvfile)
-#         #rowNum = 0
-#         for row in reader:
-#         #    rowNum += 1
-#         #    print(rowNum)
-
-#             category = row['category']
-#             tag = row['name']
-            
-#             # Add the tag to the corresponding category list
-#             if category == '5':
-#                 species.append(tag)
-#     return species
 
 # Define a handler for the /reverse_search command
 @dp.message_handler(content_types=["photo"])
@@ -89,15 +77,17 @@ async def handle_reverse_search(message: types.Message):
             await message.reply("Error: Unable to parse response")
             return
         # Send the results to the user
-        #await message.reply(json.dumps(results))
-        tags = results['tag_string'].split()
+        tags = results['posts']['tag_string'].split()
         post_tags = []
-        #speciesTags = tag_categories()
+
         for tag in tags:
-            if tag in TAG_SPECIES:                
-                post_tags.append(tag)  
-        await message.reply('https://e621.net/posts/' + str(results['id']) + '\n' +
-            ' '.join('#' + tag_to_print for tag_to_print in post_tags))
+            if tag in TAG_SPECIES:
+                post_tags.append(tag)
+        post_url = 'https://e621.net/posts/' + str(results['posts']['id'])
+        md5 = results['md5']
+        post_tags_str = ' '.join('#' + tag_to_print for tag_to_print in post_tags)
+        message_reply = f'{post_url}\nMD5: {md5}\n{post_tags_str}'
+        await message.reply(message_reply)
     else:
         # Send an error message
         await message.reply("Please send an image file to perform a reverse search.")
